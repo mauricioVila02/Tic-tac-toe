8e63392bbf4c33c5c6f5510c366c9ca11782c9f0
Mauricio Vila <arleyvila02@gmail.com>
2023-01-23 15:21:12 -0500

    add cross component

---
+++ src/components/cross/cross.tsx
@@ -0,0 +1,19 @@
+import classNames from "classnames";
+import { IFigure } from "../../global/interfaces";
+import FigureContainer from "../figureContainer/figureContainer";
+import "./../../styles/cross.css";
+
+const Cross = (props: IFigure) => {
+  const { color = "blue" } = props;
+  return (
+    <FigureContainer
+      {...props}
+      className="mv-cross position-relative d-flex justify-content-center"
+    >
+      <div style={{ color }} />
+      <div style={{ color }} />
+    </FigureContainer>
+  );
+};
+
+export default Cross;
